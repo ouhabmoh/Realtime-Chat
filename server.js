9c2999b2179ec2e5bd8c740ad0b085620c0ba3a2
ouhab <mohamed-moh98@live.com>
2023-03-08 23:55:40 +0100

    Add User Func

--- server.js
+++ server.js
@@ -14,24 +14,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 const botName = "ChatCord Bot";
 
 io.on('connection', socket =>{
-   
-
+   socket.on('joinRoom', ({username, room}) =>{
     socket.emit('message', formatMessage(botName, 'Welcome to chat cord'));
 
     // Broadcast when a user connects
     socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
+ 
+   });
 
+    
+
+    
+    //listen forchat messages
+    socket.on('chatMessage', msg => {
+        
+        io.emit('message', formatMessage('USER', msg));
+    });
 
     // Runs when user disconnects
     socket.on('disconnect', () => {
         io.emit('message', formatMessage(botName,'User disconnected'));
     });
 
-    //listen forchat messages
-    socket.on('chatMessage', msg => {
-        
-        io.emit('message', formatMessage('USER', msg));
-    });
 });
 
 const PORT = 3000 || process.env.PORT;
